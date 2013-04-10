073bbac1bb35acf5133af7b283e794c74561ee5e
Michael Poplin <mpoplin@elinkdesign.com>
2013-04-10 09:55:25 -0400

    Extended core bundle class

---
+++ PaymentSlimCDBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace TPM\PaymentSlimCDBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Payment Slim CD Bundle
+ *
+ * @author Michael Poplin <thepoplin@gmail.com>
+ */
+class PaymentSlimCDBundle extends Bundle
+{
+
+}
