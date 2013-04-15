3504caa3ebdc93fb992c915376b2679db6345649
Michael Poplin <mpoplin@elinkdesign.com>
2013-04-15 11:23:37 -0400

    Added ability to conditionally apply validation constraints on payment fields; updated configuration as needed

--- Form/CreditCardType.php
+++ Form/CreditCardType.php
@@ -2,32 +2,112 @@
 
 namespace TPM\Payment\SlimCDBundle\Form;
 
+use TPM\Payment\SlimCDBundle\Form\EventListener\FullNameFieldSubscriber;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 class CreditCardType extends AbstractType
 {
+    private $securityContext;
+
+    public function __construct(SecurityContext $securityContext)
+    {
+        $this->securityContext = $securityContext;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('holder', 'text', array('required' => false))
-            ->add('number', 'text', array('required' => false))
+            ->addEventSubscriber(new FullNameFieldSubscriber($builder->getFormFactory()))
+            ->add('ccNumber', 'text', array(
+                'required' => false,
+                'label' => 'Card Number',
+                'max_length' => 16,
+                'validation_constraint' => new Assert\NotBlank(),
+                'invalid_message' => 'Please enter a %num%-digit number.',
+                'invalid_message_parameters' => array('%num%' => 16),
+                'error_bubbling' => true
+            ))
             ->add('expires', 'date', array(
                 'required' => false,
                 'label' => 'Expiration Date',
                 'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'empty_value' => '',
+                'validation_constraint' => new Assert\NotBlank(),
+                'invalid_message' => 'Please enter a valid expiration date.',
+                'error_bubbling' => true
             ))
-            ->add('code', 'text', array('required' => false))
-
-            'value'   => new \DateTime('now'),
-                'type'    => 'date',
-                'validation' => new Assert\Date(),
-                'options' => array(
-                    'label' => 'To',
-                    'widget' => 'single_text',
-                    'error_bubbling' => true
-                ),
+            ->add('security_code', 'text', array(
+                'required' => false,
+                'label' => 'CVV Code',
+                'validation_constraint' => new Assert\NotBlank(),
+                'invalid_message' => 'Please enter a valid expiration date.',
+                'error_bubbling' => true
+            ))
         ;
+
+        $user = $this->securityContext->getToken()->getUser();
+
+        if (!$user) {
+            throw new AccessDeniedException(
+                'No authenticated user was found.'
+            );
+        }
+
+        // $factory = $builder->getFormFactory();
+
+        // $builder->addEventListener(
+        //     FormEvents::PRE_SET_DATA,
+        //     function(FormEvent $event) use($user, $factory) {
+        //         $form = $event->getForm();
+
+        //         $formOptions = array(
+        //             'class' => 'Application\Sonata\UserBundle\Entity\User',
+        //             'multiple' => false,
+        //             'expanded' => false,
+        //             'property' => 'fullName',
+        //             'query_builder' => function(EntityRepository $er) use ($user) {
+        //                 return;
+        //             },
+        //         );
+
+        //         $form->add($factory->createNamed('foo', 'entity', null, $formOptions));
+        //     }
+        // );
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+    }
+
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            // 'data_class'      => 'Sylius\Bundle\SalesBundle\Order', // or whatever likely implements this
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token',
+            'intention'       => 'credit_card',
+            'validation_groups' => function(FormInterface $form) {
+                // $data = $form->getData();
+                // if (Entity\ORDER::TYPE_CARDHOLDER == $data->getType()) {
+                //     return array('client');
+                // } else {
+                //     return array('firm');
+                // }
+            },
+        ));
     }
 
     public function getName()
---
+++ Form/EventListener/FullNameFieldSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace TPM\Payment\SlimCDBundle\Form\EventListener;
+
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class FullNameFieldSubscriber implements EventSubscriberInterface
+{
+    private $factory;
+
+    public function __construct(FormFactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(FormEvents::PRE_SET_DATA => 'preSetData');
+    }
+
+    public function preSetData(FormEvent $event)
+    {
+        $data = $event->getData();
+        $form = $event->getForm();
+
+        // normally we'll want some sort of condition here
+        $form->add($this->factory->createNamed('fullName', 'text', null, array(
+            'required' => false,
+            'label' => 'Cardholder\'s Name',
+            'validation_constraint' => new Assert\NotBlank(),
+            'error_bubbling' => true
+        )));
+    }
+}
