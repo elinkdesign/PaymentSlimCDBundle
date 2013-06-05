9249351f640e2108ad4cc8db05a29c84cd438203
Michael Poplin <thepoplin@gmail.com>
2013-06-05 12:27:21 -0400

    Missing namespace(s) fixed

--- Form/CheckType.php
+++ Form/CheckType.php
@@ -3,7 +3,13 @@
 namespace eLink\Payment\SlimCDBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class CheckType extends AbstractType
 {
