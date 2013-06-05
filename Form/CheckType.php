29010004a78bd759afbeb7850e14be4220f89d8e
Michael Poplin <thepoplin@gmail.com>
2013-06-05 12:22:16 -0400

    Jazzing up fields

--- Form/CheckType.php
+++ Form/CheckType.php
@@ -9,7 +9,14 @@ class CheckType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('checkNumber', 'text', array('required' => false));
+        $builder
+            ->add('checkNumber', 'text', array(
+                'required' => false,
+                'label' => 'Check Number',
+                'validation_constraint' => new Assert\NotBlank(),
+                'error_bubbling' => true
+            ))
+        ;
     }
 
     public function getName()
--- Form/ECheckType.php
+++ Form/ECheckType.php
@@ -10,9 +10,24 @@ class ECheckType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('accountNumber', 'text', array('required' => false))
-            ->add('routingNumber', 'text', array('required' => false))
-            ->add('checkNumber', 'text', array('required' => false))
+            ->add('accountNumber', 'text', array(
+                'required' => false,
+                'label' => 'Account Number',
+                'validation_constraint' => new Assert\NotBlank(),
+                'error_bubbling' => true
+            ))
+            ->add('routingNumber', 'text', array(
+                'required' => false,
+                'label' => 'Routing Number',
+                'validation_constraint' => new Assert\NotBlank(),
+                'error_bubbling' => true
+            ))
+            ->add('checkNumber', 'text', array(
+                'required' => false,
+                'label' => 'Check Number',
+                'validation_constraint' => new Assert\NotBlank(),
+                'error_bubbling' => true
+            ))
         ;
     }
 
